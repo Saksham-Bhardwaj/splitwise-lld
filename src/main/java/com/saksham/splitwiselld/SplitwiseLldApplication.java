b6ef4436e512f40fd5ca89ddb3156229bf7035a2
saksham <sakshambhardwaj002@gmail.com>
2024-04-29 23:33:47 +0530

    initialised repo

---
+++ src/main/java/com/saksham/splitwiselld/SplitwiseLldApplication.java
@@ -0,0 +1,13 @@
+package com.saksham.splitwiselld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseLldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saksham/splitwiselld/SplitwiseLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.splitwiselld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseLldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
